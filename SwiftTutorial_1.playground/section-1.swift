d1f941797786b7e7e1a24371068f7062a2040b83
55011212157Nay <student@tests-iMac-6.local>
2014-08-15 16:50:52 +0700

    อิอิ

---
+++ SwiftTutorial_1.playground/section-1.swift
@@ -0,0 +1,20 @@
+let tutorialteam = 56
+let editorialteam = 23
+var totalteam = tutorialteam+editorialteam
+
+
+totalteam += 1
+let priceintinferred = -19
+let priceintexplicit :Int = -19
+
+let pricedoubleinferred = -19.99
+let pricedoubleexplicit : Double = -19.99
+
+let pricefloatinferred = -19.99
+let pricefloatexplicit : Float = -19.99
+
+let onsaleinferred = true
+let onsaleexplicit:Bool = false
+
+let nameinferred = "whoopie Cushion"
+let nameExplicit:String = "whoopie Cushion"
