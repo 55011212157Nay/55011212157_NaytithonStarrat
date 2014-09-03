b5b552cd9e82c6d5b5a4637196d242e90bcaf8c5
55011212157Nay <student@tests-iMac-6.local>
2014-09-03 15:19:06 +0700

    addFunction

--- Car.playground/section-1.swift
+++ Car.playground/section-1.swift
@@ -2,8 +2,7 @@ class showroomcar
 {
     let username :String
     let car :String
-    var userID :[String:String]=["champ":"370z"]
-    
+    var userID :[String:String]
     init (username : String,car :String)
     {
         self.username = username
@@ -13,9 +12,38 @@ class showroomcar
     
     func addusername(username :String,car :String){
         userID[username] = car
-    
     }
+    
+    func countuser(){
+        var total = userID.count
+        println("Username & Car =\(total)")
+    }
+    
+    func addCar(alluser:Dictionary<String,String>){
+        
+        //    for (var i=0 ; i<.count;i++)
+        
+        for(key,value) in alluser
+        {
+            userID [key] = value
+            println("key: \(key) value:\(value)")
+        }
+        for username in alluser.keys {
+            println("Username: \(username)")
+        }
+        for car in alluser.values{
+            println("Car: \(car)")
+        }
+    }
+    
 }
-var showroom = showroomcar(username:"Nay", car:"PCX")
-showroom.addusername("Jay", car: "ford")
+
+
+var showroom = showroomcar(username:"Nay", car:"Forza")
+showroom.addusername("Ice", car: "Wave")
+showroom.addusername("Gle", car: "EBella")
+showroom.addusername("Champ", car: "Alfa")
 showroom.userID
+
+showroom.countuser()
+showroom.addCar(["gjrgjrgjr":"fgkdopfhj","sdfsdf":"sdgsdg"])
